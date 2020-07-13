7f444b703055cc94f235fe9173a30dc1bbb45428
tsuayumu <t.ayu.1605@gmail.com>
2020-07-14 00:10:23 +0900

    ツイート反映

--- app/controllers/game_comment_controller.rb
+++ app/controllers/game_comment_controller.rb
@@ -1,3 +1,5 @@
+require 'twitter_client'
+
 class GameCommentController < ApplicationController
   def index
     @date = Date.parse(params[:date])
@@ -18,24 +20,43 @@ class GameCommentController < ApplicationController
   def create
     date = Date.parse(params[:date])
     team = Team.find(params[:team_id])
-    game_comment = GameComment.create!(
-      team: team,
-      date: params[:date],
-      text: params[:text]
-    )
 
-    tweet_text = ''
-    tweet_text << "#{game_comment.text}\r"
-    request_url = "http://sports-memory.com/game/#{team.name_en}/#{date.strftime('%Y%m%d')}"
-    tweet_text << "\rもっと見る\r#{request_url} \r\r"
-    tweet_text << "##{team.name} ##{date.strftime("%-m月%-d日")}の試合中コメント"
-    
-    twitter_client.update(tweet_text) if game_comment.text.size >= 8
+    if params[:text] =~ /^ツイート/
+      twitter_client.search(params[:text].delete("ツイート")).take(5).each do |tw|
+        begin
+          GameComment.create!(
+            team: team,
+            date: params[:date],
+            text: tw.full_text
+          )
+        rescue ActiveRecord::StatementInvalid
+        end
+      end
 
-    render json: {
-      text: game_comment.text,
-      comment_at: game_comment.created_at
-    }
+      render json: {
+        text: GameComment.last.text,
+        comment_at: GameComment.last.created_at
+      }
+    else
+      game_comment = GameComment.create!(
+        team: team,
+        date: params[:date],
+        text: params[:text]
+      )
+
+      tweet_text = ''
+      tweet_text << "#{game_comment.text}\r"
+      request_url = "http://sports-memory.com/game/#{team.name_en}/#{date.strftime('%Y%m%d')}"
+      tweet_text << "\rもっと見る\r#{request_url} \r\r"
+      tweet_text << "##{team.name} ##{date.strftime("%-m月%-d日")}の試合中コメント"
+
+      twitter_client.update(tweet_text) if game_comment.text.size >= 8
+
+      render json: {
+        text: game_comment.text,
+        comment_at: game_comment.created_at
+      }
+    end
   end
   
   private
